9caa3fe3b1bdb4bd92afc44eaf72356019eb1e7d
liangshijian <liangshijian@baidu.com>
2019-07-03 10:56:40 +0800

    添加添加缓存和去掉缓存的方法

--- go/webapp/main.go
+++ go/webapp/main.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gomodule/redigo/redis"
 )
 
 //c/s协议返回的json的结构体
@@ -24,6 +25,10 @@ import (
 //  }
 //
 
+const (
+	praise_count_cache_key_fmt string = "resource_%d_prasie_cache"
+)
+
 type Response struct {
 	Errno  int    `json:"errno"`
 	ErrMsg string `json:"errmsg"`
@@ -32,7 +37,6 @@ type Response struct {
 type Item struct {
 	ResourceID int64 `json:"resourceID"`
 	Count      int64 `json:"count"`
-	Ctime      int64 `json:"ctime"`
 }
 
 var dbAddr string
@@ -64,6 +68,14 @@ func getPrasieCount(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	praiseCount, err := getFromCache(resourceID)
+	if err != nil && err != redis.ErrNil {
+		returnErrMsg(w, -1, fmt.Sprintf("%v", err))
+		return
+	}
+	if err != redis.ErrNil {
+
+	}
 	dbOpenCommand := fmt.Sprintf("test:test@tcp(%s:%d)/praise?charset=utf8", dbAddr, port)
 	db, err := sql.Open("mysql", dbOpenCommand)
 
@@ -73,7 +85,7 @@ func getPrasieCount(w http.ResponseWriter, req *http.Request) {
 	}
 	defer db.Close()
 
-	sql := fmt.Sprintf("select count,ctime from praise_count where resource_id = %d", resourceID)
+	sql := fmt.Sprintf("select count from praise_count where resource_id = %d limit 0,1", resourceID)
 	rows, sqlerr := db.Query(sql)
 	if sqlerr != nil {
 		returnErrMsg(w, -1, fmt.Sprintf("%v", sqlerr))
@@ -82,11 +94,11 @@ func getPrasieCount(w http.ResponseWriter, req *http.Request) {
 
 	defer rows.Close()
 	res := Response{Errno: 0}
-	var count, ctime int64
+	var count int64
 	if rows.Next() {
-		rows.Scan(&count, &ctime)
+		rows.Scan(&count)
 	}
-	res.Data = &Item{ResourceID: resourceID, Count: count, Ctime: ctime}
+	res.Data = &Item{ResourceID: resourceID, Count: count}
 
 	retBytes, _ := json.Marshal(res)
 
@@ -123,6 +135,34 @@ func returnErrMsg(w http.ResponseWriter, errno int, errmsg string) {
 
 	retBytes, _ := json.Marshal(Response{Errno: errno, ErrMsg: errmsg})
 	io.WriteString(w, string(retBytes))
+}
+
+//将点赞数放入redis缓存中
+func setToCache(resourceID int64, praiseCount int64) error {
+	conn, err := redis.Dial("tcp", "redis:3304")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	key := fmt.Sprintf(praise_count_cache_key_fmt, resourceID)
+	_, err = conn.Do("SET", key, praiseCount)
+	return err
+
+}
+
+//从redis中获取点赞数
+func getFromCache(resourceID int64) (int64, error) {
+	conn, err := redis.Dial("tcp", "redis:3304")
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+	key := fmt.Sprintf(praise_count_cache_key_fmt, resourceID)
+	praiseCount, err := redis.Int64(conn.Do("GET", key))
+	if err != nil {
+		return 0, err
+	}
+	return praiseCount, nil
 }
 
 func getResourceIDFromGet(req *http.Request) (int64, error) {
